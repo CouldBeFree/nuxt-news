5ea068d98cb8b4940e7f41b13791b335dbea4792
CouldBeFree <propokerstar90@gmail.com>
2019-07-27 12:30:20 +0300

    styled index page

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,22 +12,27 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons' }
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#9ccc65', height: '5px' },
   /*
   ** Global CSS
   */
   css: [
+    { src: 'vue-material/dist/vue-material.min.css', lang: 'css'},
+    { src: '~/assets/theme', lang: 'scss'}
   ],
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~/plugins/vue-material' },
+    { src: '~/plugins/axios' }
   ],
   /*
   ** Nuxt.js dev-modules
@@ -38,7 +43,22 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
   ],
+  axios: {
+    proxy: true,
+    credentials: true
+  },
+  proxy: {
+    "/api/": {
+      target: "https://newsapi.org/v2/",
+      pathRewrite: { "^/api/": "" }
+    }
+  },
+  env: {
+    NEWS_API_KEY: '63c4e6a1eb4f447cbd27b346e4cd968c'
+  },
   /*
   ** Build configuration
   */
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,58 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-news
-      </h1>
-      <h2 class="subtitle">
-        A news application build with Nuxt 2 and Firestore
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="md-layout md-alignment-center">
+    <div class="md-layout-item md-size-95">
+      <md-content class="md-layout md-gutter" style="background: #007998; padding: 1em;">
+        <ul v-for="headline in headlines" :key="headline.id" class="md-layout-item md-large-size-25 md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+          <md-card style="margin-top: 1em;" md-with-hover>
+            <md-ripple>
+              <md-card-media md-ratio="16:9">
+                <img :src="headline.urlToImage" :alt="headline.title">
+              </md-card-media>
+              <md-card-header>
+                <div class="md-title">
+                  <a :href="headline.url" target="_blank">{{headline.title}}</a>
+                </div>
+                <div>
+                  {{headline.source.name}}
+                  <md-icon class="small-icon">book</md-icon>
+                </div>
+                <div class="md-subhead" v-if="headline.author">
+                  {{headline.author}}
+                  <md-icon class="small-icon">face</md-icon>
+                </div>
+                <div class="md-subhead">
+                  {{headline.publishedAt}}
+                  <md-icon class="small-icon">alarm</md-icon>
+                </div>
+              </md-card-header>
+              <md-card-content>{{headline.description}}</md-card-content>
+              <md-card-actions>
+                <md-button class="md-icon-button">
+                  <md-icon>bookmark</md-icon>
+                </md-button>
+                <md-button class="md-icon-button">
+                  <md-icon>message</md-icon>
+                </md-button>
+              </md-card-actions>
+            </md-ripple>
+          </md-card>
+        </ul>
+      </md-content>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+    export default {
+        async asyncData({ app }){
+            const topHeadlines = await app.$axios.$get('/api/top-headlines?country=us');
+            return { headlines: topHeadlines.articles }
+        }
+    }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style scoped>
+  .small-icon{
+    font-size: 18px !important;
+  }
 </style>
